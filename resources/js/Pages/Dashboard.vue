412cbe8d5dd6ce121661b02b88919c845e10a2b6
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-09-25 14:36:27 +0800

    feat: dashboard partial

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,8 +14,22 @@ import { GithubIcon } from '@/Components/Icons/brands'
             </div>
         </template>
 
-        <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-            You're logged in!
+        <div class="p-6 overflow-hidden bg-white rounded-xl shadow-md dark:bg-gray-700">
+            BANNER
+        </div>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 my-6">
+            <div class="grid grid-cols-1 md:grid-cols-2 rounded-xl bg-gradient-to-b from-warning-300 to-warning-500">
+                <div class="p-5 space-y-2">
+                    <p class="text-white text-base font-semibold">
+                        Wallet Balance
+                    </p>
+                    <p class="text-white text-[28px] font-semibold">
+                        $ {{ $page.props.auth.user.cash_wallet }}
+                    </p>
+                </div>
+            </div>
+            <div class="bg-gray-700">123</div>
         </div>
     </AuthenticatedLayout>
 </template>
