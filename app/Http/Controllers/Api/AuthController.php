85b018abba9e3c50857d5b0710a8b6272311bd6d
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-12-08 10:43:39 +0800

    fix: return validation message for app

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules;
 use Carbon\Carbon;
 
@@ -24,63 +25,60 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'email' => 'required|string|email',
             'password' => 'required|string',
-        ]);
-        $credentials = $request->only('email', 'password');
-
-        $token = auth()->guard('api')->attempt($credentials);
-
-        if (!$token) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized',
-            ], 401);
-        }
-
-        $user = auth()->guard('api')->user();
-        if (!$user->email_verified_at) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Email is not verified.',
-            ], 401);
-        }
-
-        $user_data = [
-            'name' => $user->name,
-            'email_verified' => Carbon::parse($user->email_verified_at)->format('Y-m-d h:m:s'),
-        ];
-
-        return response()->json([
-            'status' => 'success',
-            'user' => $user_data,
-            'authorisation' => [
-                'token' => $token,
-                'type' => 'bearer',
-            ]
+        ])->setAttributeNames([
+            'email' => trans('public.Email'),
+            'password' => trans('public.Password'),
         ]);
 
+        if (!$validator->passes()){
+            return response()->json([
+                'status' => 'fail',
+                'error' => $validator->errors()->toArray()
+            ]);
+        } else {
+            $credentials = $request->only('email', 'password');
+
+            $token = auth()->guard('api')->attempt($credentials);
+
+            if (!$token) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Unauthorized',
+                ], 401);
+            }
+
+            $user = auth()->guard('api')->user();
+            if (!$user->email_verified_at) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Email is not verified.',
+                ], 401);
+            }
+
+            $user_data = [
+                'name' => $user->name,
+                'email_verified' => Carbon::parse($user->email_verified_at)->format('Y-m-d h:m:s'),
+            ];
+
+            return response()->json([
+                'status' => 'success',
+                'user' => $user_data,
+                'authorisation' => [
+                    'token' => $token,
+                    'type' => 'bearer',
+                ]
+            ]);
+        }
     }
 
     public function register(Request $request)
     {
-
-        $settingCountries = SettingCountry::all();
-
-        $formattedCountries = $settingCountries->map(function ($country) {
-            return [
-                'value' => $country->name_en,
-                'label' => $country->name_en,
-                'phone_code' => $country->phone_code,
-            ];
-        });
-
-        $validCountries = $formattedCountries->pluck('value')->toArray();
-
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => 'required|regex:/^[a-zA-Z0-9\p{Han}. ]+$/u|max:255',
-            'country' => 'required|in:' . implode(',', $validCountries),
+            'country' => 'required',
             'email' => 'required|string|email|max:255|unique:' . User::class,
             'phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:8|unique:' . User::class,
             'address_1' => 'required',
@@ -90,31 +88,68 @@ class AuthController extends Controller
             'identity_number' => 'required|unique:users,identity_number',
             'proof_front' => 'nullable',
             'proof_back' => 'nullable',
-            'referral_code' => 'nullable',
+            'referral_code' => 'required|exists:users,referral_code',
             'terms' => 'accepted',
+        ])->setAttributeNames([
+            'name' => trans('public.Name'),
+            'country' => trans('public.Country'),
+            'email' => trans('public.Email'),
+            'phone' => trans('public.Mobile Phone'),
+            'address_1' => trans('public.Address 1'),
+            'password' => trans('public.Password'),
+            'verification_type' => trans('public.Verification Via'),
+            'identity_number' => trans('public.Identification No'),
+            'proof_front' => trans('public.Proof of Identity (FRONT)'),
+            'proof_back' => trans('public.Proof of Identity (BACK)'),
+            'referral_code' => trans('public.Referrer Code'),
+            'terms' => trans('public.Terms & Conditions'),
         ]);
 
-        if($request->has('referral_code'))
-        {
-            $referral_code = $request->input('referral_code');
+        if (!$validator->passes()){
+            return response()->json([
+                'status' => 'fail',
+                'error' => $validator->errors()->toArray()
+            ]);
+        } else {
+            $user = null;
 
-            $check_referral_code = User::where('referral_code', $referral_code)->first();
+            if ($request->has('referral_code')) {
+                $referral_code = $request->input('referral_code');
 
-            if($check_referral_code)
-            {
-                $upline_id = $check_referral_code->id;
+                $check_referral_code = User::where('referral_code', $referral_code)->first();
 
-                // if($check_referral_code->hierarchyList != null)
-                // {
-                //     $upline_hierarchy = $check_referral_code->hierarchyList;
-                // }
+                if ($check_referral_code) {
+                    $upline_id = $check_referral_code->id;
 
-                if(empty($check_referral_code['hierarchyList'])){
-                    $hierarchyList = "-" . $upline_id . "-";
-                } else {
-                    $hierarchyList = $check_referral_code['hierarchyList'] . $upline_id . "-";
+                    // if($check_referral_code->hierarchyList != null)
+                    // {
+                    //     $upline_hierarchy = $check_referral_code->hierarchyList;
+                    // }
+
+                    if(empty($check_referral_code['hierarchyList'])){
+                        $hierarchyList = "-" . $upline_id . "-";
+                    } else {
+                        $hierarchyList = $check_referral_code['hierarchyList'] . $upline_id . "-";
+                    }
+
+                    $user = User::create([
+                        'name' => $request->name,
+                        'phone' => $request->phone,
+                        'email' => $request->email,
+                        'country' => $request->country,
+                        'address_1' => $request->address_1,
+                        'address_2' => $request->address_2,
+                        'password' => Hash::make($request->password),
+                        'verification_type' => $request->verification_type,
+                        'identity_number' => $request->identity_number,
+                        'upline_id' => $upline_id,
+                        'hierarchyList' => $hierarchyList,
+                        'setting_rank_id' => 1,
+                        'role' => 'user',
+                        'kyc_approval' => 'unverified',
+                    ]);
                 }
-
+            } else {
                 $user = User::create([
                     'name' => $request->name,
                     'phone' => $request->phone,
@@ -125,75 +160,57 @@ class AuthController extends Controller
                     'password' => Hash::make($request->password),
                     'verification_type' => $request->verification_type,
                     'identity_number' => $request->identity_number,
-                    'upline_id' => $upline_id,
-                    'hierarchyList' => $hierarchyList,
                     'setting_rank_id' => 1,
                     'role' => 'user',
                     'kyc_approval' => 'unverified',
                 ]);
             }
-        } else {
-            $user = User::create([
-                'name' => $request->name,
-                'phone' => $request->phone,
-                'email' => $request->email,
-                'country' => $request->country,
-                'address_1' => $request->address_1,
-                'address_2' => $request->address_2,
-                'password' => Hash::make($request->password),
-                'verification_type' => $request->verification_type,
-                'identity_number' => $request->identity_number,
-                'setting_rank_id' => 1,
-                'role' => 'user',
-                'kyc_approval' => 'unverified',
+
+            if ($request->hasFile('proof_front')) {
+                $file = $request->file('proof_front');
+
+                $originalFilename = $file->getClientOriginalName();
+                $proof_front = $file->storeAs('uploads/user/kyc', $originalFilename, 'public');
+                $path = storage_path('/app/public/' . $proof_front);
+                if (file_exists($path)) {
+                    $user->clearMediaCollection('front_identity');
+                    $user->addMedia($path)->toMediaCollection('front_identity');
+                    $user->update([
+                        'kyc_approval' => 'pending'
+                    ]);
+                }
+            }
+
+            if ($request->hasFile('proof_back')) {
+                $file = $request->file('proof_back');
+
+                $originalFilename = $file->getClientOriginalName();
+                $proof_back = $file->storeAs('uploads/user/kyc', $originalFilename, 'public');
+                $path = storage_path('/app/public/' . $proof_back);
+                if (file_exists($path)) {
+                    $user->clearMediaCollection('back_identity');
+                    $user->addMedia($path)->toMediaCollection('back_identity');
+                    $user->update([
+                        'kyc_approval' => 'pending'
+                    ]);
+                }
+            }
+
+            Wallet::create([
+                'user_id' => $user->id,
+                'name' => 'Internal Wallet'
+            ]);
+
+            $user->setReferralId();
+
+            event(new Registered($user));
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User created successfully',
+                'user' => $user,
             ]);
         }
-
-        if ($request->hasFile('proof_front')) {
-            $file = $request->file('proof_front');
-
-            $originalFilename = $file->getClientOriginalName();
-            $proof_front = $file->storeAs('uploads/user/kyc', $originalFilename, 'public');
-            $path = storage_path('/app/public/' . $proof_front);
-            if (file_exists($path)) {
-                $user->clearMediaCollection('front_identity');
-                $user->addMedia($path)->toMediaCollection('front_identity');
-                $user->update([
-                    'kyc_approval' => 'pending'
-                ]);
-            }
-        }
-
-        if ($request->hasFile('proof_back')) {
-            $file = $request->file('proof_back');
-
-            $originalFilename = $file->getClientOriginalName();
-            $proof_back = $file->storeAs('uploads/user/kyc', $originalFilename, 'public');
-            $path = storage_path('/app/public/' . $proof_back);
-            if (file_exists($path)) {
-                $user->clearMediaCollection('back_identity');
-                $user->addMedia($path)->toMediaCollection('back_identity');
-                $user->update([
-                    'kyc_approval' => 'pending'
-                ]);
-            }
-        }
-
-        Wallet::create([
-            'user_id' => $user->id,
-            'name' => 'Internal Wallet'
-        ]);
-
-        $user->setReferralId();
-
-        event(new Registered($user));
-
-        return response()->json([
-            'status' => 'success',
-            'message' => 'User created successfully',
-            'user' => $user,
-        ]);
-
     }
 
     public function logout()
