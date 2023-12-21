5647e650650254b64aef5f91166564d19e8a17fa
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-12-21 18:30:43 +0800

    feat: subscribe api

--- app/Http/Controllers/Api/EarnController.php
+++ app/Http/Controllers/Api/EarnController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\InvestmentPlan;
 use App\Models\InvestmentSubscription;
+use App\Models\Wallet;
+use App\Services\RunningNumberService;
 use Carbon\Carbon;
+use Illuminate\Validation\ValidationException;
 
 class EarnController extends Controller
 {
@@ -70,5 +73,80 @@ class EarnController extends Controller
         });
 
         return response()->json($investmentSubscriptions);
+    }
+
+    public function subscribe(\Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'wallet_id' => ['required'],
+            'amount' => ['required', 'numeric', 'integer'],
+            'unit_number' => ['sometimes', 'required'],
+            'housing_price' => ['sometimes', 'required', 'numeric', 'integer'],
+            'terms' => ['accepted'],
+        ])->setAttributeNames([
+            'wallet_id' => 'Wallet',
+            'amount' => 'Amount',
+            'unit_number' => 'Unit Number',
+            'housing_price' => 'Housing Price',
+            'terms' => 'Terms & Conditions',
+        ]);
+
+        if (!$validator->passes()){
+            return response()->json([
+                'status' => 'fail',
+                'error' => $validator->errors()->toArray()
+            ]);
+        } else {
+            $user = \Auth::user();
+            $investment_plan = InvestmentPlan::find($request->investment_plan_id);
+            $wallet = Wallet::find($request->wallet_id);
+            $amount = $request->amount;
+
+            if ($amount % 100 !== 0) {
+                throw ValidationException::withMessages(['amount' => 'Please enter an amount in increments of 100.']);
+            }
+
+            if ($amount < $investment_plan->investment_min_amount) {
+                throw ValidationException::withMessages(['amount' => 'Amount minimum is $ ' . $investment_plan->investment_min_amount]);
+            }
+
+            if ($wallet->balance < $amount) {
+                return redirect()->back()->with('title', 'Insufficient Balance')->with('warning', 'The selected wallet does not have enough balance to subscribe the investment plan. Please try again.');
+            } else {
+                $updated_balance = $wallet->balance - $amount;
+
+                $wallet->update([
+                    'balance' => $updated_balance
+                ]);
+            }
+
+            $subscription_id = RunningNumberService::getID('investment');
+
+            $investmentSubscription = InvestmentSubscription::create([
+                'user_id' => $user->id,
+                'wallet_id' => $request->wallet_id,
+                'investment_plan_id' => $investment_plan->id,
+                'subscription_id' => $subscription_id,
+                'amount' => $amount,
+                'unit_number' => $request->unit_number ?? null,
+                'unit_price' => $request->housing_price ?? null,
+                'total_earning' => 0.00,
+            ]);
+
+            $cooling_period_date = $investmentSubscription->created_at->addDays(60)->startOfDay();
+            $next_roi_date = $cooling_period_date->copy()->addMonth()->startOfMonth();
+            $expired_date = $cooling_period_date->copy()->addMonths($investment_plan->investment_period)->endOfMonth();
+
+            $investmentSubscription->update([
+                'next_roi_date' => $next_roi_date,
+                'expired_date' => $expired_date,
+            ]);
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'The selected investment plan has been subscribed successfully.',
+                'subscription' => $investmentSubscription
+            ]);
+        }
     }
 }
