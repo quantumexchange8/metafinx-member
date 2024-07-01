80ffff3de6bfd85385e091634c24cbc50f53288c
darren <darren.kooyz@gmail.com>
2024-07-01 13:46:03 +0800

    fix: payment config

--- config/payment-gateway.php
+++ config/payment-gateway.php
@@ -3,9 +3,7 @@
 return [
     'staging' => [
         'paymentUrl' => 'http://ttpayment-gateway.currenttech.pro/',
-        // 'apiKey' => 'b2680272-958b-41a1-bd97-d52ad9fcbd41',
         'appId' => 'ttpay',
-        // 'endPoint' => 'https://apilist.tronscanapi.com/api/contracts/smart-contract-triggers-batch?fields=hash,method',
         'merchantId' => '1'
     ],
     'live' => [
