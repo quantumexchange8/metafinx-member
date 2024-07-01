9a6072e7ef5c16b313516290ca9e40236c9889e0
darren <darren.kooyz@gmail.com>
2024-07-01 15:49:02 +0800

    fix: http error

--- config/payment-gateway.php
+++ config/payment-gateway.php
@@ -2,12 +2,11 @@
 
 return [
     'staging' => [
-        'paymentUrl' => 'http://ttpayment-gateway.currenttech.pro/',
+        'paymentUrl' => 'https://ttpayment-gateway.currenttech.pro/',
         'appId' => 'ttpay',
         'merchantId' => '1'
     ],
     'live' => [
-        'apiKey' => 'b2680272-958b-41a1-bd97-d52ad9fcbd41',
         'appId' => 'metafinx',
     ],
 ];
